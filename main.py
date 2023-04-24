8acfad192d5526ac3d646362ce3868a9f5f66467
rcivan <115499871+rcivan@users.noreply.github.com>
2023-04-24 18:56:05 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,125 @@
+class BinarySearchTree:
+
+    def __init__(self):
+        self.root = None
+
+    # Insert something into the tree.
+    def insert(self, val):
+        if self.root is None:
+            self.root = Node(val)
+        else:
+            self.root.insert(val)
+
+    # Check if a value is in the tree.
+    def contains(self, val):
+        if self.root is None:
+            return False
+        else:
+            return self.root.contains(val)
+
+    # Return all the items in the tree as a sorted list.
+    def toList(self):
+        if self.root is None:
+            return []
+        else:
+            return self.root.toList()
+
+    def largest(self):
+      if self.root is None:
+          return None
+      else:
+          return self.root.largest()
+
+
+class Node:
+    def __init__(self, val, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+    def getVal(self):
+        return self.val
+
+    def setVal(self, newVal):
+        self.val = newVal
+
+    def getLeft(self):
+        return self.left
+
+    def getRight(self):
+        return self.right
+
+    def setLeft(self, newLeft):
+        self.left = newLeft
+
+    def setRight(self, newRight):
+        self.right = newRight
+
+    # Insert something into the tree.
+    def insert(self, val):
+        if self.val is None:
+            self.val = val
+            return
+
+        if val < self.val:
+            if self.left is None:
+                self.left = Node(val)
+            else:
+                self.left.insert(val)
+        else:
+            if self.right is None:
+                self.right = Node(val)
+            else:
+                self.right.insert(val)
+
+    def contains(self, val):
+        if self.val == val:
+            return True
+        if val < self.val:
+            if self.left is None:
+              return False
+            else:
+              return self.left.contains(val)
+        if val > self.val:
+            if self.right is None:
+              return False
+            else:
+              return self.right.contains(val)
+
+    def largest(self):
+      if self.right is None:
+        return self.val
+      else:
+        return self.right.largest()
+
+  
+    # Return all the items in the tree as a sorted list.
+    def toList(self):
+      if self.left is None:
+        leftList = []
+      else:
+        leftList = self.left.toList()
+        
+      if self.right is None:
+        rightList = []
+      else:
+        rightList = self.right.toList()
+        
+      return leftList + [self.val] + rightList
+
+
+def main():
+  s = input("Enter a list of numbers, seperated by spaces: ")
+  lst = s.split()
+
+  tree = BinarySearchTree()
+  for x in lst:
+    tree.insert(float(x))
+
+  print(tree.toList())
+  print(tree.contains(7))
+  print(tree.largest())
+  
+
+if __name__ == "__main__":
+    main()
